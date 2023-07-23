2d6a367c069b81fb8f9235496d263a045ee7dcc1
MaryWamz <132351470+MaryWamz@users.noreply.github.com>
2023-07-23 15:37:39 +0300

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * is_printable - Evaluates if a char is printable
- * @c: Char to be evaluated.
+ * is_printable - Accesses if a char is printable
+ * @c: Char to be accessed.
  *
- * Return: 1 if c is printable, 0 otherwise
+ * Return: 1 if c is printable, otherwise 0
  */
 int is_printable(char c)
 {
@@ -15,9 +15,9 @@ int is_printable(char c)
 }
 
 /**
- * append_hexa_code - Append ascci in hexadecimal code to buffer
- * @buffer: Array of chars.
- * @i: Index at which to start appending.
+ * append_hexa_code - Include ascci in hexadecimal code to buffer
+ * @buffer: Array of characters.
+ * @i: Index to start appending.
  * @ascii_code: ASSCI CODE.
  * Return: Always 3
  */
@@ -38,10 +38,10 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 }
 
 /**
- * is_digit - Verifies if a char is a digit
- * @c: Char to be evaluated
+ * is_digit - Prooves if a char is a digit
+ * @c: Char to be accessed
  *
- * Return: 1 if c is a digit, 0 otherwise
+ * Return: 1 if c is a digit, otherwise 0
  */
 int is_digit(char c)
 {
@@ -52,11 +52,11 @@ int is_digit(char c)
 }
 
 /**
- * convert_size_number - Casts a number to the specified size
- * @num: Number to be casted.
- * @size: Number indicating the type to be casted.
+ * convert_size_number - Assigns a number to the specified size
+ * @num: Number to be assigned.
+ * @size: Number indicating the type to be assigned.
  *
- * Return: Casted value of num
+ * Return: Assigned value of num
  */
 long int convert_size_number(long int num, int size)
 {
@@ -69,11 +69,11 @@ long int convert_size_number(long int num, int size)
 }
 
 /**
- * convert_size_unsgnd - Casts a number to the specified size
- * @num: Number to be casted
- * @size: Number indicating the type to be casted
+ * convert_size_unsgnd - Assigns a number to the specified size
+ * @num: Number to be assigned
+ * @size: Number indicating the type to be assigned
  *
- * Return: Casted value of num
+ * Return: Assigned value of num
  */
 long int convert_size_unsgnd(unsigned long int num, int size)
 {

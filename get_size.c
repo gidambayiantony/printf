4bf3f603c7dc799cb29088e574acf7b16bc04958
MaryWamz <132351470+MaryWamz@users.noreply.github.com>
2023-07-23 15:04:51 +0300

    Update get_size.c

--- get_size.c
+++ get_size.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * get_size - Calculates the size to cast the argument
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
+ * get_size - Computes the size to cast the argument
+ * @format: Formats a string in which to print arguments
+ * @i: prints a list of arguments.
  *
  * Return: Precision.
  */
